9adc7209632ddbed64eb6953c5a9c89681a618e1
shane95146 <66512908+shane95146@users.noreply.github.com>
2020-07-05 16:24:29 +0800

    JS練習

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ console.log('JS');
 
 document.getElementById("box").innerHTML='哈囉2';
 document.getElementById("box").innerHTML='哈囉555';
+
+var a=10;
+a**=3;
+console.log('A**3='+a);
+
+function AAA(){
+ console.log('');
+}
